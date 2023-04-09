ed15dd2a0fd44042223ae5247d9c75fb6b9c4c58
gaay4229 <mosharafhilwa@gmail.com>
2023-04-09 12:54:30 +0300

    success

---
+++ STUDENT-MANAGMENT-SYSTEM/students/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import student
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = student
+        felids = ['student_number', 'first_name', 'last_name', 'email', 'feild_of_study','gpa']
+        labels = {
+            'student_number' 'Student Number', 
+            'first_name' 'First Name', 
+            'last_name''Last Name', 
+            'email' 'Email', 
+            'feild_of_study' 'Feild Of Study',
+            'gpa' 'GPA', 
+        }
+
+        widgets = {
+            'Student Number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'first_name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'feild_of_study': forms.TextInput(attrs={'class': 'form-control'}),
+            'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+
+
+
