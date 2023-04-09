5a86d85c9acb4d5a5358a1281035f5eaf6ddc8f9
gaay4229 <mosharafhilwa@gmail.com>
2023-04-09 19:29:11 +0300

    Success

--- samplecrud/app/models.py
+++ samplecrud/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Grade(models.Model):
+    name = models.CharField(max_length=11)
+
+class Student(models.Model):
+    name = models.CharField(max_length=40)
+    tell = models.CharField(max_length=30)
+    grade = models.ForeignKey(Grade,on_delete=models.CASCADE)
