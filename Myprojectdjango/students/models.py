b4a48165aff227fd8c8ccacc908e2bd876120653
gaay4229 <mosharafhilwa@gmail.com>
2023-04-11 14:51:05 +0300

    success

--- Myprojectdjango/students/models.py
+++ Myprojectdjango/students/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):  
+    studnet_number = models.IntegerField()
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    field_of_study = models.CharField(max_length=20)
+    gpa = models.FloatField()
+
+
+def __str__(self):
+    return f'Student: {self.first_name} {self.last_name}'
