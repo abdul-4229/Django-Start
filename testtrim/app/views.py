d4108245dcc394d0c6d6e8b5594fb2244fc7f73d
gaay4229 <mosharafhilwa@gmail.com>
2023-05-02 11:32:46 +0300

    page links

--- testtrim/app/views.py
+++ testtrim/app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+def fastfood(request):
+    return render(request,'fastfood.html')
+def brunch(request):
+    return render(request,'brunch.html')
