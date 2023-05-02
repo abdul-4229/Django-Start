da70cd41d7fef0c945d5a80a8559acea168db6cd
gaay4229 <mosharafhilwa@gmail.com>
2023-05-02 14:40:19 +0300

    page links

--- testtrim/app/views.py
+++ testtrim/app/views.py
@@ -8,3 +8,7 @@ def fastfood(request):
     return render(request,'fastfood.html')
 def brunch(request):
     return render(request,'brunch.html')
+def staff_expenses(request):
+    return render(request,'staff_expenses.html')
+def service_expenses(request):
+    return render(request,'service_expenses.html')
