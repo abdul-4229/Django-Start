5bc6567c8074be1df175bb3092bf2c08f5659c68
gaay4229 <mosharafhilwa@gmail.com>
2023-05-02 14:40:04 +0300

    page links

--- testtrim/app/urls.py
+++ testtrim/app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('fastfood', views.fastfood, name='fastfood'),
     path('brunch', views.brunch, name='brunch'),
+    path('staff_expenses', views.staff_expenses, name='staff_expenses'),
+    path('service_expenses', views.service_expenses, name='service_expenses'),
     
 ]
