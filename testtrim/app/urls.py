984f4fef22d0be6a5af494684ea306ee0398d109
gaay4229 <mosharafhilwa@gmail.com>
2023-05-03 09:41:33 +0300

    commit

--- testtrim/app/urls.py
+++ testtrim/app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('brunch', views.brunch, name='brunch'),
     path('staff_expenses', views.staff_expenses, name='staff_expenses'),
     path('service_expenses', views.service_expenses, name='service_expenses'),
+    path('utility_bills', views.utility_bills, name='utility_bills'),
     
 ]
