e4723a2d09cbc94ffc9ad1c9bf55bc8f7e053524
gaay4229 <mosharafhilwa@gmail.com>
2023-05-03 21:49:43 +0300

    commit

--- testtrim/app/urls.py
+++ testtrim/app/urls.py
@@ -3,10 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('fastfood', views.fastfood, name='fastfood'),
-    path('brunch', views.brunch, name='brunch'),
-    path('staff_expenses', views.staff_expenses, name='staff_expenses'),
-    path('service_expenses', views.service_expenses, name='service_expenses'),
-    path('utility_bills', views.utility_bills, name='utility_bills'),
+    path('addstudent', views.addstudent, name='addstudent'),
     
 ]
