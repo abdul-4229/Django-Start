ad042d4d0174184f31c746fd268ef4cf305d2f5e
gaay4229 <mosharafhilwa@gmail.com>
2023-05-05 21:02:51 +0300

    commit

---
+++ CoffeProject/app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def order(request):
+    return render(request,'order.html')
+
+def saler(request):
+    return render(request,'saler.html')
