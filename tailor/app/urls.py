032192db93953a9b762dbd553f660818d19df0b4
gaay4229 <mosharafhilwa@gmail.com>
2023-05-07 22:28:49 +0300

    commit

--- tailor/app/urls.py
+++ tailor/app/urls.py
@@ -3,6 +3,8 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('local', views.local, name='local'),
+    path('size', views.size, name='size'),
     
     
 ]
