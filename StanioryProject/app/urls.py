092a77e75668fdbdebd0d50a1592793ab050befd
gaay4229 <mosharafhilwa@gmail.com>
2023-05-08 22:04:07 +0300

    success

--- StanioryProject/app/urls.py
+++ StanioryProject/app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from app.Code import Books,Author
+from app.Code import Books,Author,Saler
 from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('book', Books.listBooks, name='book'),
     path('author', Author.listAuthor, name='author'),
+    path('saler', Saler.listSaler, name='saler'),
     
 ]
