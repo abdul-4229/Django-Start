6b6d857fe9bd2c3eed595656f90934bcbf55a748
gaay4229 <mosharafhilwa@gmail.com>
2023-05-17 00:23:09 +0300

    success

--- testtrim/app/views.py
+++ testtrim/app/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
-def addstudent(request):
-    return render(request,'addstudent.html')
+def book(request):
+    return render(request,'book.html')
