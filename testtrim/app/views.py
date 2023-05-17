e7fd63f03c297c4fda42ddd198422705e1721113
gaay4229 <mosharafhilwa@gmail.com>
2023-05-17 11:36:05 +0300

    success

--- testtrim/app/views.py
+++ testtrim/app/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
 def book(request):
     return render(request,'book.html')
+
+def author(request):
+    return render(request,'author.html')
+
+def sale(request):
+    return render(request,'sale.html')
