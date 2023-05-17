d77d2598e6d9074a7988e8d7af33b24ab8c87e2c
gaay4229 <mosharafhilwa@gmail.com>
2023-05-17 11:35:37 +0300

    success

--- testtrim/app/urls.py
+++ testtrim/app/urls.py
@@ -4,5 +4,7 @@ from app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('book', views.book, name='book'),
+    path('author', views.author, name='author'),
+    path('sale', views.sale, name='sale'),
     
 ]
